de2b7ed4914b2a06bd2368840dbc7c0389a68929
Frank Donnellan <frankiedon67@gmail.com>
2023-06-18 18:50:06 +0100

    Added routing and live data from API.

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react"; 
 import Header from "../components/headerMovieList";
 import Grid from "@mui/material/Grid";
 import MovieList from "../components/movieList";
@@ -10,17 +10,34 @@ const styles = {
 };
 
 const MovieListPage = (props) => {
-  const movies = props.movies;
+    const [movies, setMovies] = useState([]);
+    console.log(import.meta.env)
+  
+    useEffect(() => {
 
-  return (
-    <Grid container sx={styles.root}>
-      <Grid item xs={12}>
-        <Header title={"Home Page"} />
-      </Grid>
-      <Grid item container spacing={5}>
-        <MovieList movies={movies}></MovieList>
-      </Grid>
-    </Grid>
-  );
-};
+      fetch(
+        `https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-us&include_asult=false&page=1`
+      )
+        .then((res) => res.json())
+        .then((json) => {
+           console.log(json);
+          return json.results;
+        })
+        .then((movies) => {
+          setMovies(movies);
+        });
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return (
+        <Grid container sx={styles.root}>
+          <Grid item xs={12}>
+            <Header title={"Home Page"} />
+          </Grid>
+          <Grid item container spacing={5}>
+            <MovieList movies={movies}></MovieList>
+          </Grid>
+        </Grid>
+      );
+    };
 export default MovieListPage;
